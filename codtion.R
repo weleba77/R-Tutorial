077364ed8d4efbd23cadd9dd91ae4e1c276126bc
weleba77 <ephremweleba@gmail.com>
2026-06-03 10:48:29 +0300

    this is about if/else, loops(for) and functions

---
+++ codtion.R
@@ -0,0 +1,24 @@
+#################### if 
+x = 5
+if (x != 5){
+  print('hi')
+  
+}else{
+  print ('helo')
+}
+############################ functions in R  
+x = c(1,2,3,4,5,6,7,8)
+
+check_number <- function(num_val) {
+  if (num_val == 5) {
+    print('hi')
+  } else {
+    print('hello')
+  }
+}
+########################### for loop in R
+for (num in x) {
+  check_number(num)
+}
+
+  
